48fb62c7b2e49c1f3afadc3c454cd48847c1e843
raulmoes <114180098+raulmoes@users.noreply.github.com>
2023-01-10 22:43:24 +0100

    Add files via upload

---
+++ web/apache2/index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+            <?php
+               $server_ip = $_SERVER["SERVER_ADDR"];
+                echo "<h1>¡Hola, RAUL MORALES te da la bienvenida al apache 2 con ip interna ".$server_ip."</h1>";
+
+            ?>
+</html>
